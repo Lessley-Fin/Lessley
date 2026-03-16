6ee4382d56dac1eb0b4b1dfc8f9f6d1c6ee55946
Yoav Shalev <44302737+YoavJoe@users.noreply.github.com>
2026-03-16 16:02:06 +0200

    [Fix] fixing the class transaction

--- lessley-backend/Lessley.Personalization/services/utils/normalise_data.py
+++ lessley-backend/Lessley.Personalization/services/utils/normalise_data.py
@@ -1,22 +1,21 @@
 from dataclasses import dataclass
-from typing import Optional
 
 @dataclass
 class Transaction:
-    id = None
-    userId = None
-    providerId = None
-    accountNumber = None
-    status = None
-    categoryCode = None
-    ammount = None
-    description = None
-    category = None
-    type = None
-    date = None
-    merchantName = None
-    merchantAddress = None
-    createdAt = None
+    id: str | None = None
+    userId: str | None = None
+    providerId: str | None = None
+    accountNumber: str | None = None
+    status: str | None = None
+    categoryCode: str | None = None
+    ammount: float | None = None
+    description: str | None = None
+    category: str | None = None
+    type: str | None = None
+    date: str | None = None
+    merchantName: str | None = None
+    merchantAddress: str | None = None
+    createdAt: str | None = None
 
 class NormaliseData:
     
@@ -26,23 +25,10 @@ class NormaliseData:
         Normalises the input data by filtering out any unnecessary transactions data
         """
         transactions = response.json().get("items", [])
-        simplified_transactions = [
-            Transaction(
-                id = transaction.get("id"),
-                userId = transaction.get("userId"),
-                providerId = transaction.get("providerId"),
-                accountNumber = transaction.get("accountNumber"),
-                status = transaction.get("status"),
-                categoryCode = transaction.get("categoryCode"),
-                ammount = transaction.get("ammount"),
-                description = transaction.get("description"),
-                category = transaction.get("category"),
-                type = transaction.get("type"),
-                date = transaction.get("date"),
-                merchantName = transaction.get("merchantName"),
-                merchantAddress = transaction.get("merchantAddress"),
-                createdAt = transaction.get("createdAt"),
-            )
-            for transaction in transactions
-        ]
-        return simplified_transactions
+        # We filter kwargs to only include keys that exist in the dataclass
+        valid_keys = Transaction.__dataclass_fields__.keys()
+
+        transactions = [Transaction(**{k: v for k, v in t.items() if k in valid_keys})
+             for t in transactions]
+
+        return transactions
